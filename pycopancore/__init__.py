08801593efe6efd3cf83542e69b5d0d51691e1af
Jonathan Donges <donges@pik-potsdam.de>
2016-05-30 15:51:18 +0200

    Basic folder structure and files

---
+++ pycopancore/__init__.py
@@ -0,0 +1,30 @@
+# This file is part of pycopancore.
+#
+# Copyright (C) 2016 by COPAN team at Potsdam Institute for Climate
+# Impact Research
+#
+# URL: <http://www.pik-potsdam.de/copan/software>
+# License: MIT license
+
+"""
+pycopancore
+===========
+
+Subpackages
+-----------
+
+None yet.
+
+"""
+
+from x import y
+
+
+__author__ = "Jonathan F. Donges <donges@pik-potsdam.de>"
+__copyright__ = \
+    "Copyright (C) 2016 Jonathan F. Donges and COPAN team"
+__license__ = "MIT license"
+__url__ = "http://www.pik-potsdam.de/copan/software"
+__version__ = "0.1.0"
+__date__ = "2016-05-30"
+__docformat__ = "restructuredtext en"
