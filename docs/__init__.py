e809c6d1bd2731e25b4f6a0fa62c3e788d44744b
Jonathan Donges <donges@pik-potsdam.de>
2016-05-30 15:55:21 +0200

    Basic folder struct + dummy files

---
+++ docs/__init__.py
@@ -0,0 +1,8 @@
+# This file is part of pycopancore.
+#
+# Copyright (C) 2016 by COPAN team at Potsdam Institute for Climate
+# Impact Research
+#
+# URL: <http://www.pik-potsdam.de/copan/software>
+# License: MIT license
+
