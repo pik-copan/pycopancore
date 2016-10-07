1fbd150d7a343ae65a718057312b9db85c8f8e86
KilianZimmerer <kikleer@altlautern.de>
2016-10-07 16:26:06 +0200

    added logistic growth ODE  and required  variables

--- pycopancore/cell/local_one_resource.py
+++ pycopancore/cell/local_one_resource.py
@@ -13,13 +13,13 @@ Encapsulates states and dynamics of local environmental stocks.
 #
 #  Imports
 #
+
 from abstract_cell import Cell
-import numpy as np
-
 
 #
-#  Define class LocalStocks
+#  Define class Renweable_Stock
 #
+
 
 class Renewable_Stock(Cell):
     """
@@ -30,36 +30,58 @@ class Renewable_Stock(Cell):
     #  Definitions of internal methods
     #
 
+    def __init__(self,
+                 cell_identifier,
+                 coordinates,
+                 stock,
+                 capacity,
+                 growth_rate,
+                 catch_coefficient,
+                 cell_effort
+                 ):
+        """
+        Initializes an instance of a renewable stock. Inherits cell_identifier,
+        coordinates and stock from class Cell. Creates capacity, growth_rate
+        and catch_coefficient for the ODE (Wiedermann 2015).
+        """
+
+        super(Renewable_Stock, self).__init__(cell_identifier,
+                                              coordinates, stock
+                                              )
+        self.capacity = None
+        self.growth_rate = None
+        self.catch_coefficient = None
+        self.cell_effort = None
+
+    def __str__(self):
+        """
+        Returns a string representations of the object of class Renewable_Stock
+        """
+        return (super(Renewable_Stock, self).__str__() +
+                ('capacity % s, \
+                 growth rate % s, \
+                 catch coefficient % s'
+                 ) % (
+                 self.capacity,
+                 self.growth_rate,
+                 self.catch_coefficient
+                 )
+                )
+
     #
     #  Definitions of further methods
     #
 
-    def resource_function(capacity, starting_value, growthrate, time):
-        """
-        Function which returns amount of the renewable resource in dependece of
-        the capcity, the starting value, the growthrate and time based on
-        logistic growth
-        """
-        G = capacity
-        f_0 = starting_value
-        k = growthrate
-        t = time
-        R = G / (1 + np.exp(-k * G * t) * ((G / f_0) - 1))
-        return R
-
-    def renewable_stock(self, stock, capacity, growth_rate,
-                        catch_coefficient, effort
-                        ):
+    def renewable_stock(self, y, time):
         """
         The following ODE describes the stock dynamics of a renewable ressource
         through logistic growth and its harvest (Wiedermann 2015).
-        Required variables are the stock, capacity, growth_rate,
-        catch_coefficient and effort
+        Required variables are capacity, growth_rate,
+        catch_coefficient and effort. y is the stock.
         """
-        s = stock
-        K = capacity
-        a = growth_rate
-        q = catch_coefficient
-        E = effort
-        dsdt = a * s * (1 - stock / K) - q * s * E
-        return dsdt
+        a = self.growth_rate
+        K = self.capacity
+        q = self.catch_coefficient
+        E = self.cell_effort
+        dydt = a * y * (1 - y / K) - q * y * E
+        return dydt
