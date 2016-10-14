949a48fe80b845c66770c497bcf3560ea338d2e2
tillkolster <tillkolster@gmail.com>
2016-10-14 11:24:59 +0200

    metabolism begins

---
+++ pycopancore/group/abstract_metabolism.py
@@ -0,0 +1,62 @@
+# This file is part of pycopancore.
+#
+# Copyright (C) 2016 by COPAN team at Potsdam Institute for Climate
+# Impact Research
+#
+# URL: <http://www.pik-potsdam.de/copan/software>
+# License: MIT license
+
+"""
+Encapsulates states and dynamics of social metabolism.
+"""
+
+#
+#  Imports
+#
+
+#
+#  Define class Group
+#
+
+
+class Metabolism(object):
+    """
+    Encapsulates states and dynamics of connections between groups.
+    """
+
+    #
+    #  Definitions of internal methods
+    #
+
+    def __init__(self,
+                 group_connections):
+        """
+        Initialize an instance of 'Group'.
+
+        Parameters
+        ----------
+        group_connections: arrays?
+            Describes all sort of connections between groups
+        """
+
+    def __str__(self):
+        """
+        Return a string representation of the object of class individual
+        """
+        return ('group_connections % s'
+                ) % (self.group_connections)
+
+    def set_group_connections(self, connections)
+        """
+        A function to set connections.
+        
+        Parameters
+        ----------
+        connections : array?
+            This may be adjacency matrices, maybe an igraph object?
+        """
+        self.group_connections = connections
+
+    #
+    #  Definitions of further methods
+    #
