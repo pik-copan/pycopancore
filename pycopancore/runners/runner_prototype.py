7a6162c87506068e08449a9fbd69159ae1dfaeb3
tillkolster <tillkolster@gmail.com>
2016-11-24 17:16:05 +0100

    defined some functions to make runner run. Not yet shure how it will run...

--- pycopancore/runners/runner_prototype.py
+++ pycopancore/runners/runner_prototype.py
@@ -15,12 +15,13 @@ This is a model module. It only import components of the base mixins
 #
 
 from pycopancore.private import _AbstractRunner
-from pycopancore.process_types import Event, Explicit, Implicit, ODE
+from pycopancore.process_types import Event, Explicit, Implicit, ODE, Step
 from pycopancore.models import base_only
 
 #
 # Definition of class _AbstractRunner
 #
+
 
 class RunnerPrototype(_AbstractRunner):
     """
@@ -29,6 +30,7 @@ class RunnerPrototype(_AbstractRunner):
     Explicits
     Implicits
     Events
+    Steps
     """
 
     def __init__(self,
@@ -41,6 +43,7 @@ class RunnerPrototype(_AbstractRunner):
         model
         kwargs
         """
+        super(RunnerPrototype, self).__init__(**kwargs)
         self.model = model
         self.processes = (model.individual_processes + model.cell_processes
                           + model.society_processes + model.nature_processes
@@ -49,6 +52,7 @@ class RunnerPrototype(_AbstractRunner):
         self.explicit_processes = []
         self.implicit_processes = []
         self.event_processes = []
+        self.step_processes = []
         self.ode_processes = []
 
         for process in self.processes:
@@ -60,5 +64,82 @@ class RunnerPrototype(_AbstractRunner):
                 self.event_processes.append(process)
             if isinstance(process, ODE):
                 self.ode_processes.append(process)
+            if isinstance(process, Step):
+                self.step_processes.append(process)
             else:
                 print('process-type not specidfied')
+
+    def run(self,
+            t_0=0,
+            t_1=None,
+            dt=None,
+            output_variables=None
+            ):
+        """
+        This is the run-function, which calls all process-types to
+        be solved
+
+        Parameters
+        ----------
+        t_0 : float
+            starting time
+        t_1 : float
+            end-time
+        dt: float
+            time resolution
+        output_variables : list
+            list that includes all variables that shall be returned
+
+        Returns
+        -------
+        trajectory_dict: dict
+            dictionary with time and output variables as keys and list
+            with their values over the time integrated
+        """
+
+        #
+        # First: get initial values from model
+        # Second: Call discontinuity_finder to get next discontinuity
+        # Third: Calculate Values for timesteps of dt until next
+        # discontinuity is met
+        # Forth: Save them, repeat until end-time is met
+        # Fifth: Return values
+        #
+
+    def odeint_prepare(self,
+                       values):
+        """
+        Function to to be passed to odeint.
+        Parameters
+        ----------
+        values
+
+        Returns
+        -------
+        some_array : array
+            Array with all the derivatives to be passed to odeint
+        """
+
+        #
+        # This shall be the same as odeint_rhs in jobst prototype in the
+        # scipy_ODE_only_runner file. I do not yet understand it..
+        # What is this offset thing?
+        #
+
+    def discontinuitiy_finder(self):
+        """
+        Function to find and sort the discontinuities in event and
+        step type processes
+        Returns
+        -------
+        discontinuity_dict : dict
+            Dictionary with times as keys and the associated process as
+            entry
+        """
+
+        #
+        # First: Assemble all non-continuos functions
+        # Second: Call their distrinutions to see who is producing the next
+        # discontinuity
+        # Third: Return discontinuity-dictionary
+        #
