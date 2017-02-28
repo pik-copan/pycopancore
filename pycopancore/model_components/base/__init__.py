2fbdad4b3166321ded099d3f7ba6bc98367c2c7c
Jobst Heitzig <heitzig@pik-potsdam.de>
2017-02-28 11:07:24 +0100

    moved base implementation classes to subpackage as in template

--- pycopancore/model_components/base/__init__.py
+++ pycopancore/model_components/base/__init__.py
@@ -1,23 +1,13 @@
+"""
+This is the base model-component package
+"""
 # This file is part of pycopancore.
 #
-# Copyright (C) 2016 by COPAN team at Potsdam Institute for Climate Impact
+# Copyright (C) 2017 by COPAN team at Potsdam Institute for Climate Impact
 # Research
 #
 # URL: <http://www.pik-potsdam.de/copan/software>
 # License: MIT license
 
-"""
-This is the base model-component package
-"""
-from .interface import World_, Cell_, Nature_, Individual_, Culture_, \
-    Society_, Metabolism_, Model_
-
-from .world import World
-from .cell import Cell
-from .nature import Nature
-from .individual import Individual
-from .culture import Culture
-from .society import Society
-from .metabolism import Metabolism
 
 from .model import Model
