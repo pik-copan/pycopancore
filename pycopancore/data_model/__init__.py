9f79270881b2a33b00f5d60c12744c6ecc7cd581
Jobst Heitzig <heitzig@pik-potsdam.de>
2017-02-28 10:44:24 +0100

    moved Variable classes to data_model since they're not private but shall be used by model component developers

--- pycopancore/data_model/__init__.py
+++ pycopancore/data_model/__init__.py
@@ -4,3 +4,8 @@ from .dimensional_quantity import *
 
 from . import master_data_model, base_dimensions_units
 
+from .variable import Variable
+from .cets_variable import CETSVariable
+from .cf_variable import CFVariable
+from .reference_variable import ReferenceVariable
+from .set_variable import SetVariable
--- pycopancore/private/__init__.py
+++ pycopancore/private/__init__.py
@@ -11,10 +11,6 @@ This is the dummy model-component package.
 """
 
 from ._abstract_process import _AbstractProcess
-from ._abstract_dynamics_mixin import _AbstractDynamicsMixin
+from ._abstract_process_taxon_mixin import _AbstractProcessTaxonMixin
 from ._abstract_entity_mixin import _AbstractEntityMixin
 from ._abstract_runner import _AbstractRunner
-
-from .variable import Variable
-from .reference_variable import ReferenceVariable
-from .set_variable import SetVariable
