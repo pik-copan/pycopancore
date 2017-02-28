9eb8037bd0f844c2548e37e4119929bad3fdd5b4
Jobst Heitzig <heitzig@pik-potsdam.de>
2017-02-28 19:57:18 +0100

    changed inits

--- pycopancore/__init__.py
+++ pycopancore/__init__.py
@@ -2,10 +2,12 @@
 This is the pycopancore package
 """
 from .data_model import *
-#from .private import *
-from .process_types import *
-#from .models import Model # FIXME: Jobst: I don't see that this makes sense
+from . import private
 
+from .process_types import *
+from .runners import *
+
+from . import model_components, models
 
 __url__ = "http://www.pik-potsdam.de/copan/software"
 __version__ = "0.1.0"
---
+++ pycopancore/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+This is the model folder (subpackage)
+"""
+# This file is part of pycopancore.
+#
+# Copyright (C) 2017 by COPAN team at Potsdam Institute for Climate Impact
+# Research
+#
+# URL: <http://www.pik-potsdam.de/copan/software>
+# License: MIT license
