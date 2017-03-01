4c9612b8ac5860322235bdc82a29385920ecee7e
Jobst Heitzig <heitzig@pik-potsdam.de>
2017-03-01 14:43:39 +0100

    almost complete nature in master_data_model, metabolism begun

--- pycopancore/data_model/master_data_model/metabolism.py
+++ pycopancore/data_model/master_data_model/metabolism.py
@@ -1,9 +1,23 @@
-from pycopancore.data_model import Variable, CFVariable, CETSVariable
+from pycopancore.data_model import Variable
 
 # Population, demographics:
-human_population = CETSVariable(ref="???")
-# in base: Implicit(human_population == sum(human_population_by_age))
-human_population_by_age = Variable("human population by age",
-                                   "bla", array_shape=(100,))  # 1d-array
+
+population = Variable("human population", "",
+                      IAMC="Population",
+                      CETS="SP.POP",
+                      is_extensive=True, lower_bound=0)
+
+# Note: when using the following, include
+# Implicit(population == sum(population_by_age))
+population_by_age = Variable("human population by age",
+                             "(in years from 0 to 99+)",
+                             IAMC="Population",
+                             CETS="SP.POP",
+                             is_extensive=True, lower_bound=0,
+                             array_shape=(100,))  # 1d-array
+
+# Resource extraction and waste:
+
 
 # Economy:
+
