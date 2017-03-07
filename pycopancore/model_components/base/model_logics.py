c6ef6771183742f7160948f3b639987450051a66
Svenja <svenjauslu@hotmail.de>
2017-03-07 13:43:46 +0100

    UID stuff in _AbstractEntityMixin and configure stuff in model_logics as discussed.

--- pycopancore/model_components/base/model_logics.py
+++ pycopancore/model_components/base/model_logics.py
@@ -43,10 +43,10 @@ class ModelLogics (object):
 
     def __init__(self):
         if not self.__class__._configured:
-            self.configure(self.__class__)
+            self.configure()
 
     @classmethod
-    def configure(cls, mc, reconfigure=False, **kwargs):
+    def configure(cls, reconfigure=False, **kwargs):
         """Configure the model.
 
         This classmethod configures the chosen mixin model of the 'models'
@@ -56,48 +56,46 @@ class ModelLogics (object):
 
         Parameter
         ---------
-        mc : class
-            The model class which the configure method is configuring.
         reconfigure : bool
             Flag that indicates if the model should be reconfigured
         """
-        if mc._configured and not reconfigure:
+        if cls._configured and not reconfigure:
             raise ConfigureError("This model is already configured. "
                                  "Use optional argument 'reconfigure'")
 
-        mc.variables = []  # save in pairs: (variable, owning_class)
-        mc.processes = []  # save in pairs: (process, owning_class)
+        cls.variables = []  # save in pairs: (variable, owning_class)
+        cls.processes = []  # save in pairs: (process, owning_class)
 
-        mc.variables_dict = {}
+        cls.variables_dict = {}
 
-        mc.process_variables = []
+        cls.process_variables = []
 
-        mc.ODE_variables = []
-        mc.explicit_variables = []
-        mc.step_variables = []
-        mc.event_variables = []
+        cls.ODE_variables = []
+        cls.explicit_variables = []
+        cls.step_variables = []
+        cls.event_variables = []
 
-        mc.ODE_processes = []
-        mc.explicit_processes = []
-        mc.step_processes = []
-        mc.event_processes = []
+        cls.ODE_processes = []
+        cls.explicit_processes = []
+        cls.step_processes = []
+        cls.event_processes = []
 
-        print("\nConfiguring model", mc.name, "(", mc, ") ...")
+        print("\nConfiguring model", cls.name, "(", cls, ") ...")
         print("Analysing model structure...")
 
         # Construction of a list for the subsequent for-loop. It contains all
         # model mixins the model component inherits from. Only model mixins
         # that inherit from 'abstract.model' are considered. The abstract model
         # itself is excluded as well as the model component:
-        parents = list(inspect.getmro(mc))[1:]
-        mc.components = [c for c in parents
-                         if c is not abstract.Model
-                         and abstract.Model in inspect.getmro(c)
-                         ]
-        print('\nComponents:', mc.components)
+        parents = list(inspect.getmro(cls))[1:]
+        cls.components = [c for c in parents
+                          if c is not abstract.Model
+                          and abstract.Model in inspect.getmro(c)
+                          ]
+        print('\nComponents:', cls.components)
 
         # The for-loop goes through all model mixins as outlined above:
-        for c in mc.components:
+        for c in cls.components:
             interfaceclass = c.__bases__[0]
             print("Model component:", interfaceclass.name, "(", c, ")...")
             # Iterate through all entity type components the model mixin uses:
@@ -122,19 +120,19 @@ class ModelLogics (object):
                     # check if same var. object was already registered. If its
                     # codename k is different to a previous assigned codename
                     # an assertion error is the output:
-                    if v in mc.variables_dict.values():
+                    if v in cls.variables_dict.values():
                         print("            already registered by another "
                               "component")
                         assert v._codename == k, ('with Codename', k)
                     # check if same codename was already registered if not,
                     # same assertion error as above:
-                    if k in mc.variables_dict.keys():
+                    if k in cls.variables_dict.keys():
                         print("            already registered by another "
                               "component")
-                        assert mc.variables_dict[k] == v, \
+                        assert cls.variables_dict[k] == v, \
                             'Codename already in use by another variable'
                     v._codename = k
-                    mc.variables_dict[k] = v
+                    cls.variables_dict[k] = v
 
                 for p in etmixin.processes:
                     print("        Process:", p)
@@ -151,26 +149,26 @@ class ModelLogics (object):
                 for (k, v) in cvardict.items():
                     print("        Variable:", v)
                     # check if same var. object was already registered:
-                    if v in mc.variables_dict.values():
+                    if v in cls.variables_dict.values():
                         print("          already registered by another "
                               "component")
                         assert v._codename == k, ('with Codename', k)
-                    if k in mc.variables_dict.keys():
+                    if k in cls.variables_dict.keys():
                         print("          already registered by another "
                               "component")
-                        assert mc.variables_dict[k] == v, \
+                        assert cls.variables_dict[k] == v, \
                             'Codename already in use by another variable'
                     v._codename = k
-                    mc.variables_dict[k] = v
+                    cls.variables_dict[k] = v
 
                 for p in pt.processes:
                     print("        Process:", p)
 
         # Now analyse by entity type and process taxon in order to find correct
         # owning classes:
-        print('\nEntity types:', mc.entity_types)
-        print('Process taxa:', mc.process_taxa)
-        for owning_class in mc.entity_types + mc.process_taxa:
+        print('\nEntity types:', cls.entity_types)
+        print('Process taxa:', cls.process_taxa)
+        for owning_class in cls.entity_types + cls.process_taxa:
             print('    Entity-type/Process taxon:', owning_class)
             parents = list(inspect.getmro(owning_class))
             components = [c for c in parents
@@ -189,24 +187,24 @@ class ModelLogics (object):
                             }
                 for (k, v) in cvardict.items():
                     v.owning_classes.append(owning_class)
-                    if (v, owning_class) not in mc.variables:
+                    if (v, owning_class) not in cls.variables:
                         print("        Variable:", v)
-                        mc.variables.append((v, owning_class))
+                        cls.variables.append((v, owning_class))
                 for p in mixin.processes:
                     p.owning_classes.append(owning_class)
-                    if (p, owning_class) not in mc.processes:
+                    if (p, owning_class) not in cls.processes:
                         print("        Process:", p)
-                        mc.processes.append((p, owning_class))
+                        cls.processes.append((p, owning_class))
 
-        for (process, owning_class) in mc.processes:
+        for (process, owning_class) in cls.processes:
             if isinstance(process, ODE):
-                mc.ODE_processes += [(process, owning_class)]
+                cls.ODE_processes += [(process, owning_class)]
             elif isinstance(process, Explicit):
-                mc.explicit_processes += [(process, owning_class)]
+                cls.explicit_processes += [(process, owning_class)]
             elif isinstance(process, Step):
-                mc.step_processes += [(process, owning_class)]
+                cls.step_processes += [(process, owning_class)]
             elif isinstance(process, Event):
-                mc.event_processes += [(process, owning_class)]
+                cls.event_processes += [(process, owning_class)]
             else:
                 print('process-type of', process, 'not specified')
                 print(process.__class__.__name__)
@@ -215,24 +213,25 @@ class ModelLogics (object):
                 # Find the tuple (var,owc) in cls.variables with var == v
                 # to get the owning class of the variable, not necessarily
                 # the owning class of the process.
-                for (var, owc) in mc.variables:
+                for (var, owc) in cls.variables:
                     if var == v:
                         voc = owc
                 # Add the tuple (v, voc) to process_variables
-                mc.process_variables += [(v, voc)]
+                cls.process_variables += [(v, voc)]
 
                 # Add the tuple (v, voc) to the process-type_variables
                 # and (process, owning_class) to process-type_variables
-                if isinstance(process, ODE):
-                    mc.ODE_variables += [(v, voc)]
+                if isinstance(process, ODE) \
+                        and (v, voc) not in cls.ODE_variables:
+                    cls.ODE_variables += [(v, voc)]
                 elif isinstance(process, Explicit):
-                    mc.explicit_variables += [(v, voc)]
+                    cls.explicit_variables += [(v, voc)]
                 elif isinstance(process, Step):
-                    mc.step_variables += [(v, voc)]
+                    cls.step_variables += [(v, voc)]
                 elif isinstance(process, Event):
-                    mc.event_variables += [(v, voc)]
+                    cls.event_variables += [(v, voc)]
 
-        mc._configured = True
+        cls._configured = True
         print("...done")
 
     def convert_to_standard_units(self):
