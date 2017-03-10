ecfe158d0cfd1cfbe19923ab077f022eb72f261a
Jobst Heitzig <heitzig@pik-potsdam.de>
2017-03-10 14:11:13 +0100

    further units

--- pycopancore/data_model/master_data_model/dimensions_and_units.py
+++ pycopancore/data_model/master_data_model/dimensions_and_units.py
@@ -4,60 +4,74 @@ from .. import Unit
 # fundamental physical dimensions and units:
 
 length = Dimension("length", "1D spatial dimension")
-meters = Unit("meters", "meters", symbol="m", dimension=length)
-length.default_unit = kilometers = \
+m = meters = Unit("meters", "meters", symbol="m", dimension=length)
+length.default_unit = km = kilometers = \
     (meters * 1000).named("kilometers", symbol="km")
 
 time = Dimension("time", "time")
-time.default_unit = years = Unit("years", "years", symbol="a")
-seconds = (years / 31556952).named("seconds")
+time.default_unit = a = yr = years = Unit("years", "years", symbol="a")
+s = seconds = (years / 31556952).named("seconds")
 
 mass = Dimension("mass",
                  "mass (only use for matter changing its type, otherwise use specific mass dimension, e.g. 'carbon')")
-mass.default_unit = tonnes = \
+mass.default_unit = t = tonnes = \
     Unit("tonnes",
          "metric tonnes (only use for matter changing its type, otherwise use specific mass units, e.g. 'GtC')",
          symbol="t")
-gigatonnes = (tonnes * 1e9).named("gigatonnes", symbol="Gt")
-kilograms = (tonnes * 1e-3).named("kilograms", symbol="kg")
+Gt = gigatonnes = (tonnes * 1e9).named("gigatonnes", symbol="Gt")
+kg = kilograms = (tonnes * 1e-3).named("kilograms", symbol="kg")
 
 absolute_temperature = Dimension("absolute temperature",
                                  "absolute temperature")
-absolute_temperature.default_unit = kelvins = \
+absolute_temperature.default_unit = K = kelvins = \
     Unit("kelvins", "kelvins (degrees Kelvin)", symbol="K")
 
 
 # other base dimensions and units:
 
-# TODO: maybe rename to monetary value:
-value = Dimension("value",
-                  "inflation-adjusted monetary value of goods, services etc.")
-value.default_unit = dollars = US_dollars_2005 = \
+# TODO: maybe rename to monetary monetary_value:
+monetary_value = \
+    Dimension("monetary_value",
+              "inflation-adjusted monetary monetary_value of goods, services etc.")
+monetary_value.default_unit = dollars = US_dollars_2005 = \
     Unit("dollars",
          "inflation-adjusted 2005 US dollars", symbol="$")
 # TODO: also provide US_dollars_1992 etc. for conversion
 
 utility = Dimension("utility",
                     "abstract utility, representing an individual's preferences. BEWARE: this may not be considered to be interpersonally comparable!")
-utility.default_unit = utils = \
+utility.default_unit = u = utils = \
     Unit("utils", "utility points", symbol="u")
 
 carbon = Dimension("carbon",
                    "mass of carbon, whether atomic or in some chemical compound")
-carbon.default_unit = tonnes_carbon = \
+carbon.default_unit = tC = tonnes_carbon = \
     Unit("tonnes carbon",
          "tonnes of carbon, whether atomic or in some chemical compound",
          symbol="tC")
-gigatonnes_carbon = (tonnes_carbon * 1e9).named("gigatonnes carbon",
+GtC = gigatonnes_carbon = (tonnes_carbon * 1e9).named("gigatonnes carbon",
                                                 symbol="GtC")
 
 humans = Dimension("humans", "cardinality of a set of human beings")
-humans.default_unit = people = \
+humans.default_unit = H = people = \
     Unit("people", "number of human beings", symbol="H")
+
+# we do NOT treat energy as convertible to mass, so we do NOT say:
+#energy = (mass * velocity**2).named("energy")
+#joules = kilograms * meters**2 / seconds**2
+# but:
+energy = Dimension("energy", "(not convertible to mass velocity²)")
+J = joules = Unit("joules", "(not convertible to kg m²/s²)", symbol="J")
+energy.default_unit = GJ = gigajoules = \
+    (joules * 1e9).named("gigajoules", symbol="GJ")
+# TODO: provide other common units: Btu, kcal., MWh, etc.
+Btu = british_thermal_units = GJ * 0.10550559e-5
+GWh = gigawatt_hours = GJ * 3600
+Gcalth = gigacalories_th = GJ * 4.184  # thermochemical
 
 # derived flow dimensions:
 
-value_flow = (value / time).named("value flow")
+value_flow = (monetary_value / time).named("monetary_value flow")
 carbon_flow = (carbon / time).named("carbon flow")
 human_flow = (humans / time).named("human flow")
 
@@ -72,7 +86,3 @@ velocity = (length / time).named("velocity")
 
 acceleration = (velocity / time).named("acceleration")
 
-energy = (mass * velocity**2).named("energy")
-joules = kilograms * meters**2 / seconds**2
-energy.default_unit = gigajoules = \
-    (joules * 1e9).named("gigajoules", symbol="GJ")
