d77acb5537dcce9be8a34018306163308d799e4c
Jobst Heitzig <heitzig@pik-potsdam.de>
2017-03-16 16:52:16 +0100

    finally a running (unfinished) prototype! but slow...

--- studies/test_jobsts_prototype_1.py
+++ studies/test_jobsts_prototype_1.py
@@ -10,8 +10,8 @@ from pylab import plot, gca, show
 # parameters:
 
 nworlds = 1  # no. worlds
-nsocs = 1  # no. societies
-ncells = 1  # no. cells
+nsocs = 10  # no. societies
+ncells = 100  # no. cells
 
 model = M.Model()
 
@@ -26,45 +26,59 @@ worlds = [M.World(nature=nature, metabolism=metabolism,
                   ) for w in range(nworlds)]
 societies = [M.Society(world=random.choice(worlds)) for s in range(nsocs)]
 cells = [M.Cell(society=random.choice(societies)) for c in range(ncells)]
-# make references consistent:
-for c in cells:
-    c.world = c.society.world
 
 
 # distribute area and vegetation randomly but correlatedly:
 r = random.uniform(size=ncells)
 Sigma0 = 1.5e8 * D.square_kilometers * r / sum(r)
 M.Cell.land_area.set_values(cells, Sigma0)
+print(M.Cell.land_area.get_values(cells))
 
 r += random.uniform(size=ncells)
 L0 = 2480 * D.gigatonnes_carbon * r / sum(r) # 2480 is yr 2000
 M.Cell.terrestrial_carbon.set_values(cells, L0)
+print(M.Cell.terrestrial_carbon.get_values(cells))
 
 r = np.exp(random.normal(size=ncells))
 G0 = 1125 * D.gigatonnes_carbon * r / sum(r) # 1125 is yr 2000
 M.Cell.fossil_carbon.set_values(cells, G0)
+print(M.Cell.fossil_carbon.get_values(cells))
 
 r = random.uniform(size=nsocs)
 P0 = 6e9 * D.people * r / sum(r) # 500e9 is middle ages, 6e9 would be yr 2000
-M.Society.population.set_values(cells, P0)
+M.Society.population.set_values(societies, P0)
+print(M.Society.population.get_values(societies))
 
 r = random.uniform(size=nsocs)
 S0 = 1e13 * D.gigajoules * r / sum(r) # in AWS paper: 1e12 (alternatively: 1e13)
-M.Society.renewable_energy_knowledge.set_values(cells, S0)
+M.Society.renewable_energy_knowledge.set_values(societies, S0)
+print(M.Society.renewable_energy_knowledge.get_values(societies))
 
 r = random.uniform(size=nsocs)
 K0 = sum(P0) * 1e4 * D.dollars/D.people * r / sum(r) # ?
-M.Society.physical_capital.set_values(cells, K0)
+M.Society.physical_capital.set_values(societies, K0)
+print(M.Society.physical_capital.get_values(societies))
 
 # TODO: add noise to parameters
 
 runner = Runner(model=model)
 
-traj = runner.run(t_1=.01, dt=.0001)
+traj = runner.run(t_1=10, dt=.1)
 
-t = traj['t']
-plot(t, traj[M.World.atmospheric_carbon][worlds[0]])
-#plot(t, traj[M.Cell.photosynthesis_carbon_flow][cells[0]])
-plot(t, traj[M.Cell.fossil_extraction_flow][cells[0]])
+t = traj['t'][:-1]
+plot(t, traj[M.World.atmospheric_carbon][worlds[0]][:-1],"b",lw=3)
+plot(t, traj[M.World.ocean_carbon][worlds[0]][:-1],"b--",lw=3)
+plot(t, traj[M.World.terrestrial_carbon][worlds[0]][:-1],"g",lw=3)
+plot(t, traj[M.World.fossil_carbon][worlds[0]][:-1],"gray",lw=3)
+for s in societies:
+    plot(t, traj[M.Society.population][s][:-1],"yellow",lw=2)
+    plot(t, traj[M.Society.physical_capital][s][:-1],"k",lw=2)
+    plot(t, traj[M.Society.renewable_energy_knowledge][s][:-1],color="darkorange",lw=2)
+    plot(t, traj[M.Society.biomass_input_flow][s][:-1],"g--",lw=2)
+    plot(t, traj[M.Society.fossil_fuel_input_flow][s][:-1],"--",color="gray",lw=2)
+    plot(t, traj[M.Society.renewable_energy_input_flow][s][:-1],"--",color="darkorange",lw=2)
+for c in cells:
+    plot(t, traj[M.Cell.terrestrial_carbon][c][:-1],"g")
+    plot(t, traj[M.Cell.fossil_carbon][c][:-1],"gray")
 gca().set_yscale('symlog')
 show()
