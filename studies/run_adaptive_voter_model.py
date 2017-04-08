41c57c452ccdc1d35e5794a871d0ff441f92cb3b
Tim Kittel <Tim.Kittel@pik-potsdam.de>
2017-04-08 09:31:37 +0200

    adaptive voter model running now, but there seem to be problems with the event time steps

--- studies/run_adaptive_voter_model.py
+++ studies/run_adaptive_voter_model.py
@@ -3,7 +3,9 @@
 from time import time
 # from numpy import random
 import random
+import networkx as nx
 import numpy as np
+import sys
 import pycopancore.models.adaptive_voter_model as M
 # import pycopancore.models.only_copan_global_like_carbon_cycle as M
 from pycopancore import master_data_model as D
@@ -18,34 +20,39 @@ import plotly.graph_objs as go
 
 # first thing: set seed so that each execution must return same thing:
 random.seed(1)
+# TODO: figure out why it doesn't seem to work here ...
 
 # parameters:
-
-# nworlds = 1  # no. worlds
-# nsocs = 1  # no. societies
-# ncells = 1  # no. cells
-nindividuals = 100
+nindividuals = 10
 rewiring_probability = 0.1
 possible_opinions = list(range(2))
 
+
+# instantiate model
 model = M.Model()
 
-# instantiate process taxa:
-# nature = M.Nature()
-culture = M.Culture(rewiring_probability=rewiring_probability)
-# metabolism = M.Metabolism()
 
-# generate entities and plug them together at random:
+# instantiate process taxa:
+culture = M.Culture(rewiring_probability=rewiring_probability)
+
+# generate entities and distribute opinions uniformly randomly:
 world = M.World(culture=culture)
 cell = M.Cell(world=world)
-# worlds = [M.World(nature=nature, #metabolism=metabolism,
-#                   atmospheric_carbon=830 * D.gigatonnes_carbon,
-#                   ocean_carbon=(5500 - 830 - 2480 - 1125) * D.gigatonnes_carbon
-#                   ) for w in range(nworlds)]
-# societies = [M.Society(world=random.choice(worlds)) for s in range(nsocs)]
-# cells = [M.Cell(society=random.choice(societies)) for c in range(ncells)]
-individuals = [M.Individual(cell=cell, initial_opinion=random.choice(possible_opinions))]
+individuals = [M.Individual(cell=cell, initial_opinion=random.choice(possible_opinions)) for _ in range(nindividuals)]
 
+# TODO: ask Jobst, why are all individuals already in the network?
+
+def erdosrenyify(graph, p = 0.5):
+    """take a a networkx.Graph with nodes and distribute the edges following the erdos-renyi graph procedure"""
+    assert not graph.edges(), "your graph has already edges"
+    nodes = graph.nodes()
+    for i, n1 in enumerate(nodes[:-1]):
+        for n2 in nodes[i+1:]:
+            if random.random() < p:
+                graph.add_edge(n1, n2)
+
+# set the initial graph structure to be an erdos-renyi graph
+erdosrenyify(culture.acquaintance_network, p = 0.5)
 
 runner = Runner(model=model)
 
@@ -53,74 +60,43 @@ start = time()
 traj = runner.run(t_1=100, dt=.1)
 print(time()-start, " seconds")
 
-assert False, "ignore rest for now, will make proper plots with plotly when the above is running"
 
 t = np.array(traj['t'])
 print("max. time step", (t[1:]-t[:-1]).max())
 
-data_ca = go.Scatter(
+individuals_opinions = np.array([traj[M.Individual.opinion][ind] for ind in individuals])
+
+nopinion1_list = np.sum(individuals_opinions, axis=0) / nindividuals
+nopinion0_list = 1 - nopinion1_list
+
+# everything below is just plotting commands for plotly
+
+data_opinion0 = go.Scatter(
     x=t,
-    y=traj[M.World.atmospheric_carbon][worlds[0]],
-    mode="lines",
-    name="atmospheric carbon",
+    y=nopinion0_list,
+    mode="lines+markers",
+    name="relative amount opinion 0",
     line=dict(
         color="lightblue",
-        width=4
+        width=2
     )
 )
-data_ct = go.Scatter(
+data_opinion1 = go.Scatter(
     x=t,
-    y=traj[M.World.terrestrial_carbon][worlds[0]],
-    mode="lines",
-    name="terrestrial carbon",
+    y=nopinion1_list,
+    mode="lines+markers",
+    name="relative amount opinion 1",
     line=dict(
-        color="green",
-        width=4
+        color="orange",
+        width=2
     )
 )
-data_cm = go.Scatter(
-    x=t,
-    y=traj[M.World.ocean_carbon][worlds[0]],
-    mode="lines",
-    name="maritime carbon",
-    line=dict(
-        color="blue",
-        width=4
-    )
-)
-data_cf = go.Scatter(
-    x=t,
-    y=traj[M.World.fossil_carbon][worlds[0]],
-    mode="lines",
-    name="fossil carbon",
-    line=dict(
-        color="gray",
-        width=4
-    )
-)
-layout = dict(title = 'Our model (simple Carbon Cycle for now)',
-              xaxis = dict(title = 'time [yr]'),
-              yaxis = dict(title = 'Carbon [GtC]'),
+
+layout = dict(title = 'Adaptive Voter Model',
+              xaxis = dict(title = 'time'),
+              yaxis = dict(title = 'relative opinion amounts'),
               )
 
-fig = dict(data=[data_ca, data_ct, data_cm, data_cf], layout=layout)
-py.plot(fig, filename="our-model-result.html")
+fig = dict(data=[data_opinion0, data_opinion1], layout=layout)
+py.plot(fig, filename="adaptive-voter-model.html")
 
-for s in societies:
-    pass
-## the stuff below is still matplotlib style, needs to be converted to plotly
-#    plot(t, traj[M.Society.population][s],"yellow",lw=2)
-#     plot(t, traj[M.Society.physical_capital][s], "k", lw=2)
-#     plot(t, traj[M.Society.renewable_energy_knowledge][s],
-#          color="darkorange", lw=2)
-#     plot(t, traj[M.Society.biomass_input_flow][s], "g--", lw=2)
-#     plot(t, traj[M.Society.fossil_fuel_input_flow][s],
-#          "--", color="gray", lw=2)
-#     plot(t, traj[M.Society.renewable_energy_input_flow][s],
-#          "--", color="darkorange", lw=2)
-for c in cells:
-    pass
-#    plot(t, traj[M.Cell.terrestrial_carbon][c],"g")
-#    plot(t, traj[M.Cell.fossil_carbon][c],"gray")
-# gca().set_yscale('symlog')
-# show()
