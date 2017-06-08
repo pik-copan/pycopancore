400988b47986ba90c5a3c92e18e52e22e91e03cd
jakassel <kassel@pik-potsdam.de>
2017-06-08 19:05:37 +0200

    Implemented get_agent_characteristics() and get_proximity_matrix()

--- pycopancore/model_components/behave_behaviour_transition/implementation/culture.py
+++ pycopancore/model_components/behave_behaviour_transition/implementation/culture.py
@@ -16,6 +16,7 @@ from .. import interface as I
 # from .... import master_data_model as D
 # sure that's right?
 from igraph import Graph
+import numpy as np
 
 
 class Culture (I.Culture):
@@ -27,10 +28,14 @@ class Culture (I.Culture):
     __background_proximity = None
     __interaction_network = None
 
+    def __kolmogorov_smirnov_test(self):
+
+        pass
+
 
     def __init__(self,
                  # *,  # TODO: uncomment when adding named args behind here
-                 # degree_preference=None, agent_characteristics, social_influence, model_parameters (including number of individuals)
+                 # degree_preference=None, agent_characteristics, social_influence, model_parameters (including number of individuals), social_distance_function
                  **kwargs):
         """Initialize the unique instance of Culture."""
         super().__init__(**kwargs)  # must be the first line
@@ -45,6 +50,7 @@ class Culture (I.Culture):
         self.mean_degree_pref = model_parameters.mean_degree_pref
         self.std_degree_pref = model_parameters.std_degree_pref
         self.p_rew = model_parameters.p_rew
+        self.social_distance = social_distance_function
 
         # initiate contact network using degree preference
         # self.aquaintance_network = igraph.GraphBase.Erdos_Renyi(model_parametersn_individual, model_parameters.mean_degree_pref, directed=False)
@@ -60,7 +66,7 @@ class Culture (I.Culture):
         small_world_distance_matrix = np.asarray(proximity_network.shortest_paths())
 
         # Generate random noise
-        random_prox_noise = np.random.random((n_individual,n_individual))
+        random_prox_noise = np.random.random((self.n_individual,self.n_individual))
         random_prox_noise = (random_prox_noise + random_prox_noise.transpose()) * .5
 
         # Derive the background proximity matrix with added noise to generate a continuum
@@ -76,6 +82,9 @@ class Culture (I.Culture):
             __background_proximity[k,k] = 0
 
 
+        # set agents characteristics
+
+
         pass
 
     # process-related methods:
@@ -88,8 +97,33 @@ class Culture (I.Culture):
 
         pass
 
+
+    # NEED TO IMPLEMENT GET CHARACTERISTICS FUNCTION IN INDIVIDUAL!
+    # IS NUMPY ARRAY APPROPRIATE DATA TYPE?
+    def get_agents_characteristics(self):
+
+        agent_characteristics = np.array([self.n_individual])
+
+        for i in range(self.n_individual):
+            agent_characteristics[i] = self.__nodes[i].get_characteristics()
+
+        return agent_characteristics
+
+
+
+    # IS THERE A FASTER WAY FOR THIS?
+    # char_weight not yet defined
     def get_proximity_matrix(self):
-        pass
+
+        distances = np.zeros(self.n_individual, self.n_individual)
+
+        for i in range(self.n_individual):
+            for j in range(self.n_individual):
+                distances[i][j] = char_weight[0] * np.abs(np.abs(
+                    self.social_distance(self.__nodes[i],self.__nodes[j])) - 1)
+
+        # char_weight not yet defined!
+        return distances + char_weight[1] * self.__background_proximity
 
 
 
