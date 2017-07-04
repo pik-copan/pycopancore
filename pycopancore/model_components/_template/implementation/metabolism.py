2b45a796dbb4f9a34aa53e7588df363f7dd13da3
Till Kolster <tillkolster@gmail.com>
2017-07-04 14:54:51 +0200

    improved templates for validity checks

--- pycopancore/model_components/_template/implementation/metabolism.py
+++ pycopancore/model_components/_template/implementation/metabolism.py
@@ -27,6 +27,13 @@ class Metabolism (I.Metabolism):
         """Initialize the unique instance of Metabolism."""
         super().__init__(**kwargs)  # must be the first line
         # TODO: add custom code here:
+
+        # At last, check for validity of all variables that have been
+        # initialized and given a value:
+
+        # Following method is defined in abstract_process_taxon_mixin which is
+        # inherited only by mixing in the model:
+        self.assert_valid()
         pass
 
     # process-related methods:
