8ec382fab2fd0f4166ad8983e9d8c3e4c3746c5d
Till Kolster <tillkolster@gmail.com>
2017-08-15 10:01:48 +0200

    now iterates through worlds

--- pycopancore/model_components/exodus/implementation/metabolism.py
+++ pycopancore/model_components/exodus/implementation/metabolism.py
@@ -77,74 +77,74 @@ class Metabolism (I.Metabolism):
         ys = np.exp(logp_and_logys[1:])
         errors = np.zeros(shape=len(logp_and_logys))
         total_gross_income = tgi
-        # Chose a world as world. When having several, this must be changed!
+        # Iterate through worlds
         for w in self.worlds:
             world = w
-            break
-        for i, e in enumerate(world.individuals):
-            # Get the individual's society's pdf of liquidity:
-            sigma = e.society.liquidity_sigma
-            loc = e.society.liquidity_loc
-            median = e.society.liquidity_median
-            # Calculate the individual's subjective income rank:
-            sri = stats.lognorm.cdf(ys[i],
-                                    s=sigma,
-                                    loc=loc,
-                                    scale=median)
-            # Calculate the subjects nutrition
-            w_i = e.harvest - ((ys[i] - e.gross_income) / price)
-            # Get the rhs of the equation for the individual
-            errors[1 + i] = (sri - (w_i * price * stats.lognorm.pdf(ys[i],
-                                                                    s=sigma,
-                                                                    loc=loc,
-                                                                    scale=median
-                                                                    )
-                                    )
-                             ) / (2 * np.sqrt(w_i * sri))
-        # Sum over liquidity must be equal to sum over gross income:
-        errors[0] = sum(ys) - total_gross_income
-        # return rhs of the system of equations:
-        return errors
+            for i, e in enumerate(world.individuals):
+                # Get the individual's society's pdf of liquidity:
+                sigma = e.society.liquidity_sigma
+                loc = e.society.liquidity_loc
+                median = e.society.liquidity_median
+                # Calculate the individual's subjective income rank:
+                sri = stats.lognorm.cdf(ys[i],
+                                        s=sigma,
+                                        loc=loc,
+                                        scale=median)
+                # Calculate the subjects nutrition
+                w_i = e.harvest - ((ys[i] - e.gross_income) / price)
+                # Get the rhs of the equation for the individual
+                errors[1 + i] = (sri - (w_i * price * stats.lognorm.pdf(ys[i],
+                                                                        s=sigma,
+                                                                        loc=loc,
+                                                                        scale=median
+                                                                        )
+                                        )
+                                 ) / (2 * np.sqrt(w_i * sri))
+            # Sum over liquidity must be equal to sum over gross income:
+            errors[0] = sum(ys) - total_gross_income
+            # return rhs of the system of equations:
+            print('errors during solve', errors)
+            return errors
 
     def do_market_clearing(self, unused_t):
         """Calculate water price and market movements."""
         print('market cleraing takes place at time', unused_t)
-        # Chose a world as world. When having several, this must be changed!
+        # Iterate through worlds
         for w in self.worlds:
             world = w
-            break
-        # Calculate pdfs for all societies:
-        for s in world.societies:
-            s.liquidity_pdf()
-        log_liquidities = []
-        for i in world.individuals:
-            log_liquidities.append(np.log(i.liquidity))
-        logp_and_logys = [np.log(self.water_price)] + log_liquidities
-        # Get total gross income once, so that it doesn't need to be
-        # calculated each time the function is called:
-        tgi = self.total_gross_income
-        solution = optimize.root(fun=self.market_clearing_rhs,
-                                 x0=logp_and_logys,
-                                 args=(tgi)
-                                 # method='broyden1'
-                                 )
-        print(solution)
-        self.water_price = np.exp(solution['x'][0])
-        print('solution', solution)
-        for i, e in enumerate(world.individuals):
-            # Account for shift, since price of water is at first position of
-            # list, write solution of market clearing into entities:
-            e.liquidity = np.exp(solution['x'][i+1])
-            # Calculate amount of water traded:
-            # traded water = - traded money / price
-            # traded money = liquidity - gross_income
-            traded_water = - (e.liquidity - e.gross_income) / self.water_price
-            e.nutrition = e.harvest + traded_water
-        print('market clearing is done at time', unused_t,
-              'price is now at', self.water_price)
-        # Calculate liquidities again, so that sri can be calculated correctly
-        for s in world.societies:
-            s.liquidity_pdf()
+            # Calculate pdfs for all societies:
+            for s in world.societies:
+                s.liquidity_pdf()
+            log_liquidities = []
+            for i in world.individuals:
+                log_liquidities.append(np.log(i.liquidity))
+            logp_and_logys = [np.log(self.water_price)] + log_liquidities
+            # Get total gross income once, so that it doesn't need to be
+            # calculated each time the function is called:
+            tgi = self.total_gross_income
+            solution = optimize.root(fun=self.market_clearing_rhs,
+                                     x0=logp_and_logys,
+                                     args=(tgi),
+                                     method='lm'
+                                     )
+            print(solution)
+            self.water_price = np.exp(solution['x'][0])
+            print('solution', solution)
+            for i, e in enumerate(world.individuals):
+                # Account for shift, since price of water is at first position
+                # of list, write solution of market clearing into entities:
+                e.liquidity = np.exp(solution['x'][i+1])
+                # Calculate amount of water traded:
+                # traded water = - traded money / price
+                # traded money = liquidity - gross_income
+                traded_water = - (e.liquidity - e.gross_income) / self.water_price
+                e.nutrition = e.harvest + traded_water
+            print('market clearing is done at time', unused_t,
+                  'price is now at', self.water_price)
+            # Calculate liquidities again, so that sri can be calculated
+            # correctly
+            for s in world.societies:
+                s.liquidity_pdf()
 
     def market_timing(self, t):
         """Define how often market clearing takes place."""
