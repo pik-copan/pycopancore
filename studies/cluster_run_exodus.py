a95f6786dde2d1cc22403e683d70c944abaee970
Till Kolster <tillkolster@gmail.com>
2017-09-21 15:10:41 +0200

    simplified cluster script

--- studies/cluster_run_exodus.py
+++ studies/cluster_run_exodus.py
@@ -1,20 +1,5 @@
 """
-Experiment to test the influence of drought events.
-Drought events start once the civilisation has reached
-a 'complex society' state (after 200 years) and vary
-in length and severity from 0 to 100 years and 0 to 100%
-less precipitation.
-
-Therefore, starting point is at t = 200 where the model has
-reached a complex society state in all previous studies.
-We also use parameters for income from trade, agriculture and
-ecosystem services, that have previously proven to lead to
-some influence of precipitation variability on the state of the
-system.
-
-Also, we vary the parameter for income from trade so see, if
-there is a certain parameter value, that results in
-stable complex society for some drought events and collapse for others.
+A descriptive test is missing here.
 """
 
 from __future__ import print_function
@@ -31,8 +16,8 @@ from pymofa.experiment_handling import experiment_handling as eh
 import pycopancore.models.exodus as M
 from pycopancore.runners.runner import Runner
 
-from mayasim.model.ModelCore import ModelCore as Model
-from mayasim.model.ModelParameters import ModelParameters as Parameters
+# Save path
+SAVE_PATH = "p/tmp/koster/exodus_data"
 
 def run_function(timeinterval=100,
                  timestep=.1,
@@ -183,129 +168,29 @@ def run_function(timeinterval=100,
     except IOError:
         return -1
 
+# Parameter Combinations:
 
-def run_experiment(argv):
-    """
-    Take arv input variables and run sub_experiment accordingly.
-    This happens in five steps:
-    1)  parse input arguments to set switches
-        for [test],
-    2)  set output folders according to switches,
-    3)  generate parameter combinations,
-    4)  define names and dictionaries of callables to apply to sub_experiment
-        data for post processing,
-    5)  run computation and/or post processing and/or plotting
-        depending on execution on cluster or locally or depending on
-        experimentation mode.
-
-    Parameters
-    ----------
-    argv: list[N]
-        List of parameters from terminal input
-
-    Returns
-    -------
-    rt: int
-        some return value to show whether sub_experiment succeeded
-        return 1 if sucessfull.
-    """
-    # Generate paths according to switches and user name
-
-    raw = 'raw_data/'
-    res = 'results/'
-
-    save_path_res = './{}'.format(res)
-    save_path_raw = './{}'.format(raw)
-
-    # Generate parameter combinations and set up 'index' dictionary,
-    # indicating their possition in the Index of the postprocessed results.
-
-    index = {0: "d_length", 1: "d_severity", 2: "r_trade"}
-
-    number_of_municipalities = list(range(1, 5, 1))
-    number_of_counties = list(range(1, 5, 1))
-    number_of_farmers = [100, 500, 1000]
-    number_of_townsmen = [100, 500, 1000]
-    network_degree = list(range(3, 9, 2))
+number_of_municipalities = list(range(1, 5, 1))
+number_of_counties = list(range(1, 5, 1))
+number_of_farmers = [100, 500, 1000]
+number_of_townsmen = [100, 500, 1000]
+network_degree = list(range(3, 9, 2))
 
 
-    # Order of the parameters in the resulting tuples have to match the one indicated in the
-    # index dictionary.
-    param_combs = list(it.product(number_of_municipalities,
-                                  number_of_counties,
-                                  number_of_farmers,
-                                  number_of_townsmen,
-                                  network_degree))
-    print('computing results for {} parameter combinations'.format(len(param_combs)))
+# Order of the parameters in the resulting tuples have to match the one indicated in the
+# index dictionary.
+param_combs = list(it.product(number_of_municipalities,
+                              number_of_counties,
+                              number_of_farmers,
+                              number_of_townsmen,
+                              network_degree))
+print('computing results for {} parameter combinations'.format(len(param_combs)))
 
-    # In this experiment, I use the job_id variable from an array job to split the
-    # parameter combinations into equally sized junks.
-    # This makes it easier for the queing algorithm to allocate its resources (which means,
-    # it will give you more of it ;)
-    # this also means, that the total number of jobs you run, must be a divider of the
-    # number of parameter combinations that you run.
-    if len(param_combs) % max_id != 0:
-        print('number of jobs ({}) has to be multiple of max_id ({})!!'.format(len(param_combs), max_id))
-        exit(-1)
+sample_size = 1
 
-    sample_size = 1
+handle = eh(sample_size=sample_size,
+            parameter_combinations=param_combs,
+            use_kwargs=True,
+            path_res=SAVE_PATH)
 
-    # Define names and callables for post processing
-
-    name1 = "trajectory"
-    estimators1 = {"<mean_trajectories>":
-                   lambda fnames:
-                   pd.concat([np.load(f)["trajectory"] for f in fnames]).groupby(level=0).mean(),
-                   "<sigma_trajectories>":
-                   lambda fnames:
-                   pd.concat([np.load(f)["trajectory"] for f in fnames]).groupby(level=0).std()
-                   }
-    name2 = "all_trajectories"
-    estimators2 = {"trajectory_list":
-                   lambda fnames: [np.load(f)["trajectory"] for f in fnames]}
-
-    def foo(fnames, keys):
-        key = keys[0]
-        data = [np.load(f)[key] for f in fnames]
-        df = pd.DataFrame(data=data, columns=[keys[0]])
-        for key in keys[1:]:
-            data = [np.load(f)[key] for f in fnames]
-            df[key] = data
-        return df
-
-    name3 = "all_final_states"
-    estimators3 = {"final states":
-                   lambda fnames:
-                   foo(fnames, ["final population",
-                                "final trade links",
-                                "final max cluster size"])
-                   }
-
-    # Run computation and post processing.
-
-    # devide parameter combination into equally sized chunks.
-    cl = int(len(param_combs)/max_id)
-    i = (job_id-1)*cl
-    j = job_id*cl
-
-    handle = eh(sample_size=sample_size,
-                parameter_combinations=param_combs[i:j],
-                index=index,
-                path_raw=save_path_raw,
-                path_res=save_path_res,
-                use_kwargs=True)
-    if mode == 1:
-        handle.compute(run_func=run_function)
-        return 0
-    elif mode == 2:
-        handle.resave(eva=estimators1, name=name1)
-        handle.resave(eva=estimators2, name=name2)
-        handle.resave(eva=estimators3, name=name3)
-        return 0
-
-    return 1
-
-# The definition of the run_function makes it easier to test the experiment with pytest.
-if __name__ == '__main__':
-
-    run_experiment(sys.argv)
+handle.compute(run_func=run_function)
