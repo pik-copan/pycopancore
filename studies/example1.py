b7087d3f8abb158c46280eef46e022cfd2e52e82
Jobst Heitzig <konsument@posteo.de>
2017-12-27 14:32:28 +0100

    cleaned up example1 and plot code.
    7dwarfs example does not run since it lacks a social system

--- studies/example1.py
+++ studies/example1.py
@@ -1,41 +1,31 @@
-"""Skript to run Jobsts prototype model."""
+"""Script to run example1 model."""
 
 from time import time
 from numpy import random, array
 import numpy as np
+
 import pycopancore.models.example1 as M
-# import pycopancore.models.only_copan_global_like_carbon_cycle as M
 from pycopancore import master_data_model as D
 from pycopancore.runners import Runner
 
 from pylab import plot, gca, show, figure, subplot, gca, semilogy, legend
 
-# print(D.hours.factor,D.hours.exponents)
-# q=1*D.hours
-# print(q,q._number,q._unit,q.number(),q.number(unit=D.minutes),q.tostr(),q.tostr(unit=D.minutes))
-# print(q+30*D.minutes,q-15*D.minutes)
-# tens=D.unity*10
-# print((1*tens).reduce())
-# u=D.gigatonnes_carbon/D.years
-# print(u,u.factor,u.exponents)
-# q=1*u
-# print(q,q.tostr())
-# exit()
-
 # first thing: set seed so that each execution must return same thing:
-random.seed(10) # 10
+random.seed(10)
 
 # parameters:
 
 nworlds = 1  # no. worlds
-nsocs = 5 # no. social_systems #5
-ncells = 100  # no. cells #100
+nsocs = 5 # no. social_systems
+ncells = 100  # no. cells
 ninds = 1000 # no. individuals
 
 t_1 = 2120
 
-#filename = "/home/jobst/work/with.pickle"
-filename = "/home/jobst/work/without.pickle"
+# choose one of two scenarios:
+filename = "/home/jobst/work/with.pickle"
+#filename = "/home/jobst/work/without.pickle"
+# (these files will be read by plot_example1.py)
 
 with_spillovers = 1
 
@@ -55,33 +45,35 @@ model = M.Model()
 # instantiate process taxa:
 environment = M.Environment()
 metabolism = M.Metabolism(
-    renewable_energy_knowledge_spillover_fraction = .1 if with_spillovers else 0, #.1, #.1,
-        # 1 w/o protection: success but desertification
-        # .75 w protection: success (even w/o or w much migration)
-        # .1 w protection: success but desertification
-        # 0 w/o protection: very slow success but desertification
-        # ?: oscillations
-    basic_emigration_probability_rate = 16e-13 if with_migration else 0, # leads to ca. 5mio. at 5 socs, (real)
+    renewable_energy_knowledge_spillover_fraction = 
+        .1 if with_spillovers else 0,
+    basic_emigration_probability_rate = 
+        16e-13 if with_migration else 0, # leads to ca. 5mio. at 5 socs, (real)
     )
 culture = M.Culture(
-    awareness_lower_carbon_density=1e-4, #1e100, # 1e-6?
-    awareness_upper_carbon_density=2e-4, #1e100, # 2e-6?
-    awareness_update_rate = 1 if with_awareness else 0, #0, #1:SUCCESS, #1e-10
-    environmental_friendliness_learning_rate = 1 if with_learning else 0, #0, #1:SUCCESS
+    awareness_lower_carbon_density=1e-4,
+    awareness_upper_carbon_density=2e-4,
+    awareness_update_rate = 1 if with_awareness else 0,
+    environmental_friendliness_learning_rate = 1 if with_learning else 0,
     max_protected_terrestrial_carbon_share=0,
     )
 
 # generate entities and plug them together at random:
-worlds = [M.World(environment=environment, metabolism=metabolism, culture=culture,
+worlds = [M.World(environment=environment, 
+                  metabolism=metabolism, 
+                  culture=culture,
                   atmospheric_carbon = 830 * D.gigatonnes_carbon,
-                  upper_ocean_carbon = (5500 - 830 - 2480 - 1125) * D.gigatonnes_carbon
+                  upper_ocean_carbon = (5500 - 830 - 2480 - 1125) * D.GtC
                   ) for w in range(nworlds)]
-social_systems = [M.SocialSystem(world=random.choice(worlds),
-                       has_renewable_subsidy = random.choice([False, True], p=[3/4, 1/4]), #True, #False,
-                       has_emissions_tax = random.choice([False, True], p=[4/5, 1/5]), #True, #False,
-                       has_fossil_ban = False, #True, #False,
-                       time_between_votes = 4 if with_voting else 1e100, #1e100, #4, #1e100, # 4
-                       ) for s in range(nsocs)]
+social_systems = [M.SocialSystem(
+                    world=random.choice(worlds),
+                    has_renewable_subsidy = random.choice([False, True], 
+                                                          p=[3/4, 1/4]),
+                    has_emissions_tax = random.choice([False, True], 
+                                                      p=[4/5, 1/5]),
+                    has_fossil_ban = False, 
+                    time_between_votes = 4 if with_voting else 1e100, 
+                    ) for s in range(nsocs)]
 cells = [M.Cell(social_system=random.choice(social_systems),
                 renewable_sector_productivity = 2 * random.rand()
                     * M.Cell.renewable_sector_productivity.default)
@@ -89,7 +81,7 @@ cells = [M.Cell(social_system=random.choice(social_systems),
 individuals = [M.Individual(
                 cell=random.choice(cells),
                 is_environmentally_friendly = 
-                    random.choice([False, True], p=[.7, .3]), #True, #False,
+                    random.choice([False, True], p=[.7, .3]), 
                 ) 
                for i in range(ninds)]
 
@@ -108,46 +100,36 @@ for index, i in enumerate(individuals):
                 else p_samesoc if i.social_system == j.social_system \
                 else p_other):
             culture.acquaintance_network.add_edge(i, j)
-#print("degrees:",culture.acquaintance_network.degree())
 
 # distribute area and vegetation randomly but correlatedly:
 r = random.uniform(size=ncells)
 Sigma0 = 1.5e8 * D.square_kilometers * r / sum(r)
 M.Cell.land_area.set_values(cells, Sigma0)
-# print(M.Cell.land_area.get_values(cells))
 
 r += random.uniform(size=ncells)
 L0 = 2480 * D.gigatonnes_carbon * r / sum(r)  # 2480 is yr 2000
 M.Cell.terrestrial_carbon.set_values(cells, L0)
-# print(M.Cell.terrestrial_carbon.get_values(cells))
 
 r = np.exp(random.normal(size=ncells))
 G0 = 1125 * D.gigatonnes_carbon * r / sum(r)  # 1125 is yr 2000
 M.Cell.fossil_carbon.set_values(cells, G0)
-# print(M.Cell.fossil_carbon.get_values(cells))
 
 r = random.uniform(size=nsocs)
-P0 = 6e9 * D.people * r / sum(r)  # 500e9 is middle ages, 6e9 would be yr 2000
+P0 = 6e9 * D.people * r / sum(r)  # 6e9 is yr 2000
 M.SocialSystem.population.set_values(social_systems, P0)
-M.SocialSystem.migrant_population.set_values(social_systems, P0 * 250e6/6e9)
+M.SocialSystem.migrant_population.set_values(social_systems, P0 * 250e6 / 6e9)
 for s in social_systems:
-    s.max_protected_terrestrial_carbon = 0.90 * sum(c.terrestrial_carbon for c in s.cells)
-
-# print(M.SocialSystem.population.get_values(social_systems))
-   
+    s.max_protected_terrestrial_carbon = 
+        0.90 * sum(c.terrestrial_carbon for c in s.cells)
+ 
 r = random.uniform(size=nsocs)
 K0 = sum(P0) * 1e4 * D.dollars/D.people * r / sum(r)  # ?
 M.SocialSystem.physical_capital.set_values(social_systems, K0)
-# print(M.SocialSystem.physical_capital.get_values(social_systems))
 
 # for renewables, do NOT divide by number of socs:    
 r = random.uniform(size=nsocs)
-# in AWS paper: 1e12 (alternatively: 1e13):
 S0 = 1e12 * D.gigajoules * r / r.mean()
 M.SocialSystem.renewable_energy_knowledge.set_values(social_systems, S0)
-# print(M.SocialSystem.renewable_energy_knowledge.get_values(social_systems))
-
-# TODO: add noise to parameters
 
 w = worlds[0]
 s = social_systems[0]
@@ -158,14 +140,12 @@ for v in w.variables: print(v,v.get_value(w))
 for v in s.variables: print(v,v.get_value(s))
 for v in c.variables: print(v,v.get_value(c))
 
-# from pycopancore.private._expressions import eval
-# import pycopancore.model_components.base.interface as B
-# import sympy as sp
 
+# do simulation:
 runner = Runner(model=model)
-
 start = time()
-traj = runner.run(t_0=2000, t_1=t_1, dt=1, add_to_output=[M.Individual.represented_population])
+traj = runner.run(t_0=2000, t_1=t_1, dt=1, 
+                  add_to_output=[M.Individual.represented_population])
 
 
 for v in environment.variables: print(v,v.get_value(environment))
@@ -190,94 +170,46 @@ print(time()-start, " seconds")
 t = np.array(traj['t'])
 print("max. time step", (t[1:]-t[:-1]).max())
 
-figure()
-lws = 1
-
-subplot(511)  # cultural
-for i, s in enumerate(social_systems):
-#    plot(t[3:], traj[M.SocialSystem.protected_terrestrial_carbon_share][s][3:],color="green",lw=lws, label=None if i else "share of protected biomass")
-    plot(t[3:], traj[M.SocialSystem.has_renewable_subsidy][s][3:]*1,color="darkorange",lw=lws, label=None if i else "renewable subsidy in place?")
-    plot(t[3:], traj[M.SocialSystem.has_emissions_tax][s][3:]*1,color="blue",lw=lws, label=None if i else "emissions tax in place?")
-    plot(t[3:], traj[M.SocialSystem.has_fossil_ban][s][3:]*1,color="gray",lw=lws, label=None if i else "fossil ban in place?")
-legend()
-
-subplot(512)  # metabolic, logscale: capital, knowledge, production, wellbeing
-for i, s in enumerate(social_systems):
-    semilogy(t[3:], array(traj[M.SocialSystem.physical_capital][s][3:])/1e6, color="black", lw=lws, label=None if i else "physical capital [mio. $]")
-    semilogy(t[3:], array(traj[M.SocialSystem.renewable_energy_knowledge][s][3:])/1e6, color="darkorange", lw=lws, label=None if i else "renewable energy knowledge [PJ]")
-    semilogy(t[3:], array(traj[M.SocialSystem.economic_output_flow][s][3:])/1e6, color="blue", lw=lws, label=None if i else "GDP [mio. $ per year]")
-    semilogy(t[3:], array(traj[M.SocialSystem.wellbeing][s][3:]), color="magenta",lw=lws, label=None if i else "wellbeing [$ per year and person]")
-legend()
-
-subplot(513)  # metabolic: population (left), migration (right)
-ax1 = gca()
-#ax1.set_xlabel('years')
-# Make the y-axis label, ticks and tick labels match the line color.
-ax1.set_ylabel('humans')
-ax1.tick_params('y')
-ax2 = ax1.twinx()
-ax2.set_ylabel('humans per year', color='gray')
-ax2.tick_params('y', colors='gray')
-for i, s in enumerate(social_systems):
-    ax1.plot(t[3:], traj[M.SocialSystem.population][s][3:],color="black",lw=lws, label=None if i else "population")
-    ax1.plot(t[3:], traj[M.SocialSystem.migrant_population][s][3:],"-.",color="black",lw=lws, label=None if i else "migrant population")
-    ax2.plot(t[3:], traj[M.SocialSystem.immigration][s][3:],"--",color="gray",lw=lws, label=None if i else "immigration flow")
-    ax2.plot(t[3:], traj[M.SocialSystem.emigration][s][3:],":",color="gray",lw=lws, label=None if i else "emigration flow")
-ax1.legend(loc=2)
-ax2.legend(loc=1)
-
-subplot(514)  # metabolic: energy mix
-for i, s in enumerate(social_systems):
-    Es = array(traj[M.SocialSystem.secondary_energy_flow][s][3:])
-    plot(t[3:], array(traj[M.SocialSystem.biomass_input_flow][s][3:]) * metabolism.biomass_energy_density / Es, color="green", lw=lws, label=None if i else "share of biomass")
-    plot(t[3:], array(traj[M.SocialSystem.fossil_fuel_input_flow][s][3:]) * metabolism.fossil_energy_density / Es, color="gray", lw=lws, label=None if i else "share of fossils")
-    plot(t[3:], array(traj[M.SocialSystem.renewable_energy_input_flow][s][3:]) / Es, color="darkorange", lw=lws, label=None if i else "share of renewables")
-legend()
-
-subplot(515)  # natural
-plot(t[3:], traj[M.World.atmospheric_carbon][worlds[0]][3:], color="cyan", lw=3, label="atmospheric carbon")
-plot(t[3:], traj[M.World.upper_ocean_carbon][worlds[0]][3:], color="blue", lw=3, label="upper ocean carbon")
-plot(t[3:], traj[M.World.terrestrial_carbon][worlds[0]][3:], color="green", lw=3, label="terrestrial carbon")
-plot(t[3:], traj[M.World.fossil_carbon][worlds[0]][3:], color="gray", lw=3, label="fossil carbon")
-for i, s in enumerate(social_systems):
-    plot(t[3:], traj[M.SocialSystem.protected_terrestrial_carbon][s][3:],color="green",lw=lws, label=None if i else "protected biomass")
-gca().set_ylabel('gigatonnes carbon')
-legend()
-
 print("\nyr 2000 values (real):")
-print("emigration (5e6):",sum(traj[M.SocialSystem.emigration][s][5] for s in social_systems)) # should be ca. 5e6
-print("photo (123):",sum(traj[M.Cell.photosynthesis_carbon_flow][c][5] for c in cells))
-print("resp (118):",sum(traj[M.Cell.terrestrial_respiration_carbon_flow][c][5] for c in cells))
-print("GWP (4e13):",sum(traj[M.SocialSystem.economic_output_flow][s][5] for s in social_systems)) # should be ca. 4e13
-B0 = sum(traj[M.SocialSystem.biomass_input_flow][s][5] for s in social_systems)
+print("emigration (5e6):",sum(traj[M.SocialSystem.emigration][s][5] 
+                              for s in social_systems))
+print("photo (123):",sum(traj[M.Cell.photosynthesis_carbon_flow][c][5] 
+                         for c in cells))
+print("resp (118):",sum(traj[M.Cell.terrestrial_respiration_carbon_flow][c][5] 
+                        for c in cells))
+print("GWP (4e13):",sum(traj[M.SocialSystem.economic_output_flow][s][5] 
+                        for s in social_systems))
+B0 = sum(traj[M.SocialSystem.biomass_input_flow][s][5] 
+         for s in social_systems)
 Bglobal = B0 * D.gigatonnes_carbon / D.years
-Fglobal = sum(traj[M.SocialSystem.fossil_fuel_input_flow][s][5] for s in social_systems) * D.gigatonnes_carbon / D.years
-Rglobal = sum(traj[M.SocialSystem.renewable_energy_input_flow][s][5] for s in social_systems) * D.gigajoules / D.years
-Eglobal = sum(traj[M.SocialSystem.secondary_energy_flow][s][5] for s in social_systems) * D.gigajoules / D.years
+Fglobal = sum(traj[M.SocialSystem.fossil_fuel_input_flow][s][5] 
+              for s in social_systems) * D.gigatonnes_carbon / D.years
+Rglobal = sum(traj[M.SocialSystem.renewable_energy_input_flow][s][5] 
+              for s in social_systems) * D.gigajoules / D.years
+Eglobal = sum(traj[M.SocialSystem.secondary_energy_flow][s][5] 
+              for s in social_systems) * D.gigajoules / D.years
 print("B (3), F (11), R(100):",
-      Bglobal, Bglobal.tostr(unit=D.gigatonnes_carbon/D.years), # should be ca. 3
-      Fglobal, Fglobal.tostr(unit=D.gigatonnes_carbon/D.years), # should be ca. 11
-      Rglobal, Rglobal.tostr(unit=D.gigawatts), # last should be ca. 100
-      Eglobal, Eglobal.tostr(unit=D.gigawatts), # last should be ca. 100
+      Bglobal, Bglobal.tostr(unit=D.gigatonnes_carbon/D.years),
+      Fglobal, Fglobal.tostr(unit=D.gigatonnes_carbon/D.years),
+      Rglobal, Rglobal.tostr(unit=D.gigawatts),
       ) 
-#print((Bglobal * metabolism.biomass_energy_density * D.gigajoules/D.gigatonnes_carbon).tostr(unit=D.gigawatts),
-#      (Fglobal * metabolism.fossil_energy_density * D.gigajoules/D.gigatonnes_carbon).tostr(unit=D.gigawatts),
-#      Rglobal.tostr(unit=D.gigawatts), "(100)") # last should be ca. 100
-print("life exp. at end:",1/np.mean([traj[M.SocialSystem.mortality][s][-1] for s in social_systems]))
-print("cap. deprec. at begin (0.1?):",np.mean([traj[M.SocialSystem.physical_capital_depreciation_rate][s][5] for s in social_systems]))
-print("cap. deprec. at end (0.1?):",np.mean([traj[M.SocialSystem.physical_capital_depreciation_rate][s][-1] for s in social_systems]))
-print("deaths at begin (>250000?):",sum(traj[M.SocialSystem.deaths][s][5] for s in social_systems))
-print("deaths at end (>250000?):",sum(traj[M.SocialSystem.deaths][s][-1] for s in social_systems))
-print("temp. at begin:",traj[M.World.surface_air_temperature][worlds[0]][5])
-print("temp. at end:",traj[M.World.surface_air_temperature][worlds[0]][-1])
-print("prot. carbon share:",[traj[M.SocialSystem.protected_terrestrial_carbon][s][-1]/sum(traj[M.Cell.terrestrial_carbon][c][-1]for c in s.cells) for s in social_systems])
-print(traj[M.World.terrestrial_carbon][worlds[0]][-1],sum(traj[M.Cell.terrestrial_carbon][c][-1] for c in worlds[0].cells))
-#show()
-
-# TODO: 
-# policy shares, env. friendly shares
-# transparancy, means
-# wellb. abs., renew., pop. log
-# total protected
-# horiz.
-# save dump with codename,uid as key
+print("life exp. at end:", 1/np.mean([traj[M.SocialSystem.mortality][s][-1] 
+                                     for s in social_systems]))
+print("cap. deprec. at begin (0.1?):",
+      np.mean([traj[M.SocialSystem.physical_capital_depreciation_rate][s][5] 
+               for s in social_systems]))
+print("cap. deprec. at end (0.1?):",
+      np.mean([traj[M.SocialSystem.physical_capital_depreciation_rate][s][-1] 
+               for s in social_systems]))
+print("deaths at begin (>250000?):", sum(traj[M.SocialSystem.deaths][s][5] 
+                                        for s in social_systems))
+print("deaths at end (>250000?):", sum(traj[M.SocialSystem.deaths][s][-1] 
+                                      for s in social_systems))
+print("temp. at begin:", traj[M.World.surface_air_temperature][worlds[0]][5])
+print("temp. at end:", traj[M.World.surface_air_temperature][worlds[0]][-1])
+print("prot. carbon share:", 
+      [traj[M.SocialSystem.protected_terrestrial_carbon][s][-1]
+       / sum(traj[M.Cell.terrestrial_carbon][c][-1] 
+             for c in s.cells) for s in social_systems])
+print(traj[M.World.terrestrial_carbon][worlds[0]][-1],
+      sum(traj[M.Cell.terrestrial_carbon][c][-1] for c in worlds[0].cells))
--- studies/plot_example1.py
+++ studies/plot_example1.py
@@ -1,11 +1,13 @@
-"""Skript to plot Jobsts prototype model results."""
+"""Script to plot example1 model results."""
 
 import numpy as np
 from numpy import array, average, mean
-from pylab import plot, gca, show, figure, subplots, gca, semilogy, legend, tight_layout, NullLocator
+from pylab import plot, gca, show, figure, subplots, gca, semilogy, legend, \
+    tight_layout, NullLocator
 from pickle import load
 
-f, ((ax11, ax12), (ax21, ax22), (ax31, ax32)) = subplots(nrows=3, ncols=2, sharex=True, sharey=False)
+f, ((ax11, ax12), (ax21, ax22), (ax31, ax32)) = \
+    subplots(nrows=3, ncols=2, sharex=True, sharey=False)
 lws = 1
 al = 0.5
 
@@ -20,12 +22,8 @@ cells = list(traj["Cell.fossil_carbon"].keys())
 individuals = list(traj["Individual.is_environmentally_friendly"].keys())
 t = np.array(traj['t'])
 
-print(sum(array(traj["SocialSystem.protected_terrestrial_carbon"][s]) for s in social_systems)
-      / sum(array(traj["Cell.terrestrial_carbon"][c]) for c in cells))
-
 # cultural
 ax11.plot(t[3:], 100*average([array(traj["Individual.is_environmentally_friendly"][i][3:]*1) for i in individuals], axis=0,
-#                        weights=[array(traj["Individual.represented_population"][i][3:]*1) for i in individuals]
                         ),color="green",lw=lws, label="env. friendly individuals")
 ax11.plot(t[3:], 100*mean([array(traj["SocialSystem.has_renewable_subsidy"][s][3:]*1) for s in social_systems], axis=0),color="darkorange",lw=lws, label="regions w/ renewable subsidy")
 ax11.plot(t[3:], 100*mean([array(traj["SocialSystem.has_emissions_tax"][s][3:]*1) for s in social_systems], axis=0),color="blue",lw=lws, label="regions w/ emissions tax")
@@ -40,23 +38,15 @@ for i, s in enumerate(social_systems):
     ax21.plot(t[3:], 100*array(traj["SocialSystem.biomass_input_flow"][s][3:]) * 40e9 / Es, color="green", lw=lws, alpha=al, label=None if i else "biomass")
     ax21.plot(t[3:], 100*array(traj["SocialSystem.fossil_fuel_input_flow"][s][3:]) * 47e9 / Es, color="gray", lw=lws, alpha=al, label=None if i else "fossils")
     ax21.plot(t[3:], 100*array(traj["SocialSystem.renewable_energy_input_flow"][s][3:]) / Es, color="darkorange", lw=lws, alpha=al, label=None if i else "renewables")
-#plot(t[3:], 100*average([array(traj["biomass_input_flow"][s][3:]) for s in social_systems], axis=0,
-#                        weights=[array(traj["population"][s][3:]) for s in social_systems]) * 40e9 / Es, color="green", lw=3, label="global share of biomass energy")
-#plot(t[3:], 100*average([array(traj["fossil_fuel_input_flow"][s][3:]) for s in social_systems], axis=0,
-#                        weights=[array(traj["population"][s][3:]) for s in social_systems]) * 47e9 / Es, color="gray", lw=3, label="global share of fossil energy")
-#plot(t[3:], 100*average([array(traj["renewable_energy_input_flow"][s][3:]) for s in social_systems], axis=0,
-#                        weights=[array(traj["population"][s][3:]) for s in social_systems]) / Es, color="darkorange", lw=3, label="global share of renewables")
 ax21.set_ylabel('percent')
 ax21.set_ylim(-5,105)
 ax21.legend(title='MET: energy shares')
 
-# natural
+# environmental
 ax31.plot(t[3:], traj["World.atmospheric_carbon"][worlds[0]][3:], color="cyan", lw=2, label="atmosphere")
 ax31.plot(t[3:], traj["World.upper_ocean_carbon"][worlds[0]][3:], color="blue", lw=2, label="upper oceans")
 ax31.plot(t[3:], sum(array(traj["Cell.terrestrial_carbon"][c][3:]) for c in cells), color="green", lw=2, label="plants & soils")
 ax31.plot(t[3:], sum(array(traj["Cell.fossil_carbon"][c][3:]) for c in cells), color="gray", lw=2, label="fossils")
-#for i, s in enumerate(social_systems):
-#    plot(t[3:], traj["protected_terrestrial_carbon"][s][3:],color="green",lw=lws, label=None if i else "protected biomass")
 ax31.set_ylabel('gigatonnes carbon')
 ax31.set_ylim(-100,3100)
 ax31.legend(title='ENV: global carbon stocks')
@@ -84,7 +74,6 @@ ax12.plot(t[3:], 100*mean([array(traj["SocialSystem.has_emissions_tax"][s][3:]*1
 ax12.plot(t[3:], 100*mean([array(traj["SocialSystem.has_fossil_ban"][s][3:]*1) for s in social_systems], axis=0),color="gray",lw=lws, label="regions w/ fossil ban")
 ax12.set_ylim(-5,105)
 ax12.yaxis.set_major_locator(NullLocator())
-#ax12.legend()
 
 # metabolic
 for i, s in enumerate(social_systems):
@@ -92,26 +81,16 @@ for i, s in enumerate(social_systems):
     ax22.plot(t[3:], 100*array(traj["SocialSystem.biomass_input_flow"][s][3:]) * 40e9 / Es, color="green", lw=lws, alpha=al, label=None if i else "biomass")
     ax22.plot(t[3:], 100*array(traj["SocialSystem.fossil_fuel_input_flow"][s][3:]) * 47e9 / Es, color="gray", lw=lws, alpha=al, label=None if i else "fossils")
     ax22.plot(t[3:], 100*array(traj["SocialSystem.renewable_energy_input_flow"][s][3:]) / Es, color="darkorange", lw=lws, alpha=al, label=None if i else "renewables")
-#plot(t[3:], 100*average([array(traj["biomass_input_flow"][s][3:]) for s in social_systems], axis=0,
-#                        weights=[array(traj["population"][s][3:]) for s in social_systems]) * 40e9 / Es, color="green", lw=3, label="global share of biomass energy")
-#plot(t[3:], 100*average([array(traj["fossil_fuel_input_flow"][s][3:]) for s in social_systems], axis=0,
-#                        weights=[array(traj["population"][s][3:]) for s in social_systems]) * 47e9 / Es, color="gray", lw=3, label="global share of fossil energy")
-#plot(t[3:], 100*average([array(traj["renewable_energy_input_flow"][s][3:]) for s in social_systems], axis=0,
-#                        weights=[array(traj["population"][s][3:]) for s in social_systems]) / Es, color="darkorange", lw=3, label="global share of renewables")
 ax22.set_ylim(-5,105)
 ax22.yaxis.set_major_locator(NullLocator())
-#ax22.legend()
 
-# natural
+# environmental
 ax32.plot(t[3:], traj["World.atmospheric_carbon"][worlds[0]][3:], color="cyan", lw=2, label="atmosphere")
 ax32.plot(t[3:], traj["World.upper_ocean_carbon"][worlds[0]][3:], color="blue", lw=2, label="upper oceans")
 ax32.plot(t[3:], sum(array(traj["Cell.terrestrial_carbon"][c][3:]) for c in cells), color="green", lw=2, label="plants & soils")
 ax32.plot(t[3:], sum(array(traj["Cell.fossil_carbon"][c][3:]) for c in cells), color="gray", lw=2, label="fossils")
-#for i, s in enumerate(social_systems):
-#    plot(t[3:], traj["protected_terrestrial_carbon"][s][3:],color="green",lw=lws, label=None if i else "protected biomass")
 ax32.yaxis.set_major_locator(NullLocator())
 ax32.set_ylim(-100,3100)
-#ax32.legend()
 
 ax32.set_xlabel('year')
 ax32.set_xlim(1990,2110)
@@ -119,10 +98,3 @@ ax32.set_xlim(1990,2110)
 f.subplots_adjust(hspace=0, wspace=0)
 show()
 
-# TODO: 
-# policy shares, env. friendly shares
-# transparancy, means
-# wellb. abs., renew., pop. log
-# total protected
-# horiz.
-# save dump with codename,uid as key
