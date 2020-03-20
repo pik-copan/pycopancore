22bcc9d1db7d7940fa2fa61410030e17b36eaa63
Marc Wiedermann <marcwiedermann@posteo.de>
2020-03-20 22:23:22 +0100

    cleaned up the script

--- studies/esd_description_paper_example/plot_figure5.py
+++ studies/esd_description_paper_example/plot_figure5.py
@@ -1,88 +1,86 @@
 """Script to plot example2 model results."""
 from pickle import load
-import matplotlib
-matplotlib.use("Agg")
 import numpy as np
-from numpy import array, mean
 from matplotlib import pyplot as plt
 
-lws = 2
-al = 1.
-ls = ["-", ":"]
+LINEWIDTH = 2
+LINESTYLES = ["-", ":"]
 
 def plot_one_row(infile, axarr):
 
-    ax1, ax2, ax3, ax4 = axarr
+    traj = load(open(infile, "rb"))
 
-    traj = load(open(infile,"rb"))
     worlds = list(traj["World.atmospheric_carbon"].keys())
     social_systems = list(traj["SocialSystem.has_fossil_ban"].keys())
     cells = list(traj["Cell.fossil_carbon"].keys())
     individuals = list(traj["Individual.is_environmentally_friendly"].keys())
-    t = np.array(traj['t'])
-   
-    share_env_friendly = [np.array(traj["Individual.is_environmentally_friendly"][i][3:]*1) for i in individuals]
-    weights = [np.array(traj["Individual.represented_population"][i][3:]*1) for i in individuals]
-    share_env_friendly = 100 * np.average(share_env_friendly, weights=weights, axis=0)
-    ax1.plot(t[3:], share_env_friendly, color="green",lw=2, 
-              label="env. friendly individuals")
 
-    has_fossil_ban = 1 + 100*mean([array(traj["SocialSystem.has_fossil_ban"][s][3:]*1) for s in social_systems], axis=0)
-    ax1.plot(t[3:], has_fossil_ban, color="cyan",lw=2, 
-              label="regions w/subsidy and fossil ban")
+    time = np.array(traj['t'])
 
-    for i, s in enumerate(social_systems):
-        energy_flow = array(traj["SocialSystem.secondary_energy_flow"][s][3:])
-        biomass_flow = 100 * array(traj["SocialSystem.biomass_input_flow"][s][3:]) * 40e9 / energy_flow
-        fossil_flow = 100 * array(traj["SocialSystem.fossil_fuel_input_flow"][s][3:]) * 47e9 / energy_flow
-        renewable_flow = 100 * array(traj["SocialSystem.renewable_energy_input_flow"][s][3:]) / energy_flow
-        ax2.plot(t[3:], biomass_flow, color="green", ls=ls[i], lw=lws, alpha=al, label=None if i else "biomass")
-        ax2.plot(t[3:], fossil_flow, color="gray", ls=ls[i], lw=lws, alpha=al, label=None if i else "fossils")
-        ax2.plot(t[3:], renewable_flow, color="darkorange", ls=ls[i], lw=lws, alpha=al, label=None if i else "renewables")
-    
-   
-    # Left column, third row: GPD without social processes
-    for i, s in enumerate(social_systems):
-        economic_flow = 100*array(traj["SocialSystem.economic_output_flow"][s][3:]) / [0.25, 0.75][i] / 6e9
-        ax3.plot(t[3:], economic_flow, color="red", ls=ls[i], lw=lws, alpha=al, label=None if i else "GDP")
+    _y = [traj["Individual.is_environmentally_friendly"][i][3:] for i in individuals]
+    _w = [traj["Individual.represented_population"][i][3:] for i in individuals]
+    _y = 100 * np.average(_y, weights=_w, axis=0)
+    axarr[0].plot(time[3:], _y, color="green", lw=2, label="env. friendly individuals")
 
-    ax3.semilogy()
-   
-    # Left column, forth row: Environment without social processes
-    atmos_carb = traj["World.atmospheric_carbon"][worlds[0]][3:]
-    ocean_carb = traj["World.upper_ocean_carbon"][worlds[0]][3:]
-    terre_carb = sum(array(traj["Cell.terrestrial_carbon"][c][3:]) for c in cells)
-    fossi_carb = sum(array(traj["Cell.fossil_carbon"][c][3:]) for c in cells)
-    #photosynth = 10 * sum(array(traj["Cell.photosynthesis_carbon_flow"][c][3:]) for c in cells)
-    
-    print((atmos_carb+terre_carb+ocean_carb)[-1])
-    ax4.plot(t[3:], atmos_carb,
-             color="cyan", lw=2, label="atmosphere")
-    ax4.plot(t[3:], ocean_carb,
-             color="blue", lw=2, label="upper oceans")
-    ax4.plot(t[3:], terre_carb,
-             color="green", lw=2, label="plants & soils")
-    ax4.plot(t[3:], fossi_carb,
-             color="gray", lw=2, label="fossils")
-    
-   
-    ax1.set_ylim(-5,105)
-    ax2.set_ylim(-5,105)
-   
-    ax4.set_ylim(-100,3500)
-    ax4.set_xlabel('model year')
-    ax4.set_xlim(2000, 2120)
-    ax4.set_xticks(ticks=np.arange(2000, 2130, 20))
-    ax4.set_xticklabels(np.arange(0, 130, 20))
+    _y = [traj["SocialSystem.has_fossil_ban"][s][3:] for s in social_systems]
+    _y = 1 + 100* np.mean(_y, axis=0)
+    axarr[0].plot(time[3:], _y, color="cyan", lw=2, label="regions w/subsidy and fossil ban")
+
+    for i, soc in enumerate(social_systems):
+        energy_flow = np.array(traj["SocialSystem.secondary_energy_flow"][soc][3:])
+
+        _y = np.array(traj["SocialSystem.biomass_input_flow"][soc][3:])
+        _y = 100 * _y* 40e9 / energy_flow
+        axarr[1].plot(time[3:], _y, color="green", ls=LINESTYLES[i],
+                      lw=LINEWIDTH, label=None if i else "biomass")
+
+        _y = np.array(traj["SocialSystem.fossil_fuel_input_flow"][soc][3:])
+        _y = 100 * _y * 47e9 / energy_flow
+        axarr[1].plot(time[3:], _y, color="gray", ls=LINESTYLES[i],
+                      lw=LINEWIDTH, label=None if i else "fossils")
+
+        _y = np.array(traj["SocialSystem.renewable_energy_input_flow"][soc][3:])
+        _y = 100 * _y / energy_flow
+        axarr[1].plot(time[3:], _y, color="darkorange", ls=LINESTYLES[i],
+                      lw=LINEWIDTH, label=None if i else "renewables")
+
+    for i, soc in enumerate(social_systems):
+        _y = np.array(traj["SocialSystem.economic_output_flow"][soc][3:])
+        _y = 100 * _y / [0.25, 0.75][i] / 6e9
+        axarr[2].plot(time[3:], _y, color="red", ls=LINESTYLES[i],
+                      lw=LINEWIDTH, label=None if i else "GDP")
+
+    _y = traj["World.atmospheric_carbon"][worlds[0]][3:]
+    axarr[3].plot(time[3:], _y, color="cyan", lw=2, label="atmosphere")
+
+    _y = traj["World.upper_ocean_carbon"][worlds[0]][3:]
+    axarr[3].plot(time[3:], _y, color="blue", lw=2, label="upper oceans")
+
+    _y = sum(np.array(traj["Cell.terrestrial_carbon"][c][3:]) for c in cells)
+    axarr[3].plot(time[3:], _y, color="green", lw=2, label="plants & soils")
+
+    _y = sum(np.array(traj["Cell.fossil_carbon"][c][3:]) for c in cells)
+    axarr[3].plot(time[3:], _y, color="gray", lw=2, label="fossils")
+
+    axarr[0].set_ylim(-5, 105)
+    axarr[1].set_ylim(-5, 105)
+    axarr[3].set_ylim(-100, 3500)
+
+    axarr[2].semilogy()
+
+    axarr[3].set_xlabel('model year')
+    axarr[3].set_xlim(2000, 2120)
+    axarr[3].set_xticks(ticks=np.arange(2000, 2130, 20))
+    axarr[3].set_xticklabels(np.arange(0, 130, 20))
 
 def main():
 
     with_file = "esd_example_with_social_update_rate_12.0_seed_0.p"
     wo_file = "esd_example_without_social_seed_0.p"
-    
-    f, axarr = plt.subplots(nrows=4, ncols=2, sharex=True, figsize=(12, 14))
-   
-    plot_one_row(wo_file, axarr[:, 0]) 
+
+    _, axarr = plt.subplots(nrows=4, ncols=2, sharex=True, figsize=(12, 14))
+
+    plot_one_row(wo_file, axarr[:, 0])
     plot_one_row(with_file, axarr[:, 1])
 
     axarr[0, 0].title.set_text('example run without socio-cultural processes')
@@ -99,8 +97,8 @@ def main():
     axarr[3, 0].set_ylabel('ENV:\nglobal carbon stocks\n(gigatonnes carbon)')
     axarr[3, 0].legend(loc="upper left")
 
-    for ax in axarr[:, 1]:
-        plt.setp(ax.get_yticklabels(), visible=False)
+    for axs in axarr[:, 1]:
+        plt.setp(axs.get_yticklabels(), visible=False)
 
     plt.tight_layout()
     plt.savefig("figure5.pdf")
