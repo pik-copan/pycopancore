7e027868c4fb2c07eb697847e7577ee22b926cc0
zugnachpankow <bechthold.wo@gmail.com>
2023-01-16 16:26:46 +0100

    write cluster submit script

---
+++ studies/submit_parallel_maxploit_mofa.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --qos=medium
+#SBATCH --job-name=maxploit_mofa_run_parallel
+#SBATCH --account=copan
+#SBATCH --ntasks=8
+#SBATCH --output=maxploit_run-%j.out
+#SBATCH --error=maxploit_run-%j.err
+#SBATCH --workdir=/p/projects/copan/users/maxbecht/pycopancore/studies
+
+echo "------------------------------------------------------------"
+echo "SLURM JOB ID: $SLURM_JOBID"
+echo "$SLURM_NTASKS tasks"
+echo "------------------------------------------------------------"
+
+export I_MPI_PMI_LIBRARY=/p/system/slurm/lib/libpmi.so
+srun -n $SLURM_NTASKS --mpi=pmi2 python run_parallel_maxploit_mofa.py
