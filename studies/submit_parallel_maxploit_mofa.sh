1040f75987831fb9b05703338034176857f1c170
zugnachpankow <bechthold.wo@gmail.com>
2023-03-03 10:58:33 +0100

    track

--- studies/submit_parallel_maxploit_mofa.sh
+++ studies/submit_parallel_maxploit_mofa.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --qos=medium
-#SBATCH --job-name=full_norm_coarse_sweep
+#SBATCH --qos=short
+#SBATCH --job-name=descriptive_coarse_sweep
 #SBATCH --account=copan
 #SBATCH --ntasks=32
 #SBATCH --exclusive
-#SBATCH --output=full_norm_coarse_sweep-%j.out
-#SBATCH --error=full_norm_coarse_sweep-%j.err
+#SBATCH --output=descriptive_coarse_sweep-%j.out
+#SBATCH --error=descriptive_coarse_sweep-%j.err
 #SBATCH --workdir=/p/projects/copan/users/maxbecht/pycopancore/studies
 
 echo "------------------------------------------------------------"
