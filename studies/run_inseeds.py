0d4682782d173f3df2875b5de2e2b38688d1eae0
Jannes <jannesbr@pik-potsdam.de>
2023-06-30 14:20:26 +0200

    adjust inseeds run script, allow for regridding with countries, include spinups

--- studies/run_inseeds.py
+++ studies/run_inseeds.py
@@ -1,31 +1,62 @@
-"""Test run script for LPJmL coupling"""
-
-# This file is part of pycopancore.
-#
-# Copyright (C) 2022 by COPAN team at Potsdam Institute for Climate
-# Impact Research
-#
-# URL: <http://www.pik-potsdam.de/copan/software>
+"""Run script for InSEEDS with LPJmL coupling"""
 import os
-os.chdir("/p/projects/open/Jannes/copan_core/pycopancore")
-
 import numpy as np  # which is usually needed
+
 from pycoupler.config import read_config
 from pycoupler.run import run_lpjml
 from pycoupler.coupler import LPJmLCoupler
-from pycoupler.utils import check_lpjml
+from pycoupler.utils import check_lpjml, search_country
 
+os.chdir("/p/projects/open/Jannes/copan_core/pycopancore")
 # from pycopancore.runners.runner import Runner
-from pycopancore.models import social_inseeds as M
+from pycopancore.models import social_inseeds as M  # noqa
 
+
+# Settings ================================================================== #
 
 # paths
 sim_path = "/p/projects/open/Jannes/copan_core/lpjml"
 model_path = f"{sim_path}/LPJmL_internal"
+inseeds_config_file = "/p/projects/open/Jannes/copan_core/pycopancore/pycopancore/models/social_inseeds_config.yaml"  # noqa"
+
+spinup = False
+
+# search for country code by supplying country name
+# search_country("netherlands")
+country_code = "NLD"
+
+# Configuration ============================================================= #
+
+# Spinup run ---------------------------------------------------------------- #
+if spinup:
+    # create config for spinup run
+    config_spinup = read_config(file_name="lpjml.js",
+                                model_path=model_path,
+                                spin_up=True)
+
+    # set spinup run configuration
+    config_spinup.set_spinup(sim_path)
+
+    # set more recent input files
+    config_spinup.radiation = "cloudiness"
+    config_spinup.input.temp.name = "CRU_TS4.03/cru_ts4.03.1901.2018.tmp.clm"
+    config_spinup.input.prec.name = "CRU_TS4.03/cru_ts4.03.1901.2018.pre.clm"
+    config_spinup.input.cloud.name = "CRU_TS4.03/cru_ts4.03.1901.2018.cld.clm"
+    config_spinup.input.co2.name = "input_VERSION2/co2_1841-2018.dat"
+    config_spinup.input.wetdays.name = "CRU_TS4.03/cru_ts4.03.1901.2018.wet.clm"  # noqa
+    config_spinup.input.landuse.name = "input_toolbox_30arcmin/cftfrac_1500-2017_64bands_f2o.clm"  # noqa
+
+    # only for global runs = TRUE
+    config_spinup.river_routing = False
+
+    # regrid by country - create new (extracted) input files and update config
+    config_spinup.regrid(sim_path, country_code=country_code)
+
+    # write config (Config object) as json file
+    config_spinup_fn = config_spinup.to_json()
 
 
-startcell = 27410
-endcell = 27418
+# Coupled run --------------------------------------------------------------- #
 
 # create config for coupled run
 config_coupled = read_config(model_path=model_path, file_name="lpjml.js")
@@ -33,29 +64,58 @@ config_coupled = read_config(model_path=model_path, file_name="lpjml.js")
 # set coupled run configuration
 config_coupled.set_coupled(sim_path,
                            sim_name="coupled_test",
-                           start_year=1901, end_year=2005,
-                           coupled_year=1981,
+                           start_year=1901, end_year=2050,
+                           coupled_year=2023,
                            coupled_input=["with_tillage"],
                            coupled_output=["soilc",
                                            "cftfrac",
                                            "pft_harvestc",
                                            "hdate"])
 
-# only for single cell runs
-config_coupled.outputyear = 1980
-config_coupled.startgrid = startcell
-config_coupled.endgrid = endcell
+# only for single cells runs
+config_coupled.outputyear = 2022
+
+# set more recent input files
+config_coupled.radiation = "cloudiness"
+config_coupled.input.temp.name = "CRU_TS4.03/cru_ts4.03.1901.2018.tmp.clm"
+config_coupled.input.prec.name = "CRU_TS4.03/cru_ts4.03.1901.2018.pre.clm"
+config_coupled.input.cloud.name = "CRU_TS4.03/cru_ts4.03.1901.2018.cld.clm"
+config_coupled.input.co2.name = "input_VERSION2/co2_1841-2018.dat"
+config_coupled.input.wetdays.name = "CRU_TS4.03/cru_ts4.03.1901.2018.wet.clm"  # noqa
+config_coupled.input.landuse.name = "input_toolbox_30arcmin/cftfrac_1500-2017_64bands_f2o.clm"  # noqa
+config_coupled.fix_climate = True
+config_coupled.fix_climate_cycle = 11
+config_coupled.fix_climate_year = 2013
+
+# only for global runs = TRUE
 config_coupled.river_routing = False
 config_coupled.tillage_type = "read"
 config_coupled.double_harvest = False
 
-config_coupled.add_config(
-    "/p/projects/open/Jannes/copan_core/pycopancore/pycopancore/models/social_inseeds_config.yaml"  # noqa
-)
+# regrid by country - create new (extracted) input files and update config file
+config_coupled.regrid(sim_path, country_code=country_code)
+
+config_coupled.add_config(inseeds_config_file)
 
 # write config (Config object) as json file
 config_coupled_fn = config_coupled.to_json()
 
+
+# Simulations =============================================================== #
+
+# LPJmL spinup run ---------------------------------------------------------- #
+if spinup:
+    # check if everything is set correct
+    check_lpjml(config_file=config_spinup_fn, model_path=model_path)
+
+    # run spinup job
+    run_lpjml(
+        config_file=config_spinup_fn,
+        model_path=model_path,
+        sim_path=sim_path
+    )
+
+# LPJmL coupled run --------------------------------------------------------- #
 
 # check if everything is set correct
 check_lpjml(config_coupled_fn, model_path)
@@ -68,20 +128,23 @@ run_lpjml(
     std_to_file=False,  # write stdout and stderr to file
 )
 
+# InSEEDS run --------------------------------------------------------------- #
+
+# establish coupler connection to LPJmL
 lpjml = LPJmLCoupler(config_file=config_coupled_fn)
 
-
+# initialize (LPJmL) world
 world = M.World(lpjml=lpjml)
 
-
+# initialize (cells and) individuals
 farmers = world.init_individuals(model=M)
 
+# run coupled model until end_year
+for year in world.lpjml.get_sim_years():
+    world.update(year)
 
-# Runner sucks
+
+# No Runner needed
 # Runner(model=M).run(t_0=world.lpjml.config.start_coupling,
 #                         t_1=world.lpjml.config.lastyear,
 #                         dt=1)
-
-
-for year in world.lpjml.get_sim_years():
-    world.update(year)
