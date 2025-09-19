d6f918f64d1da938ffc00618af8da9fa8f2c5d60
jnnsbrr <jannesbr@LT1244.pik-potsdam.de>
2025-09-19 20:11:15 +0200

    add pypi, hatchling support and reduce readme following good practices

--- pycopancore/__init__.py
+++ pycopancore/__init__.py
@@ -10,3 +10,11 @@ This is the pycopancore package.
 # URL: <http://www.pik-potsdam.de/copan/software>
 # Contact: core@pik-potsdam.de
 # License: BSD 2-clause license
+
+try:
+    from ._version import __version__
+except ModuleNotFoundError:  # pragma: no cover
+    # package is not installed
+    __version__ = "0.8.3"
+
+__all__ = ["__version__"]
